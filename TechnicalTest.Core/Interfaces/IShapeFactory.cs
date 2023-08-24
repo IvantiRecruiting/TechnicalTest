a819ea4935909a42973fa10cca81d22ab828431b
Adam Ball <adam.ball@n-able.com>
2023-08-24 11:45:36 +0100

    removed whitespace

--- TechnicalTest.Core/Interfaces/IShapeFactory.cs
+++ TechnicalTest.Core/Interfaces/IShapeFactory.cs
@@ -5,6 +5,7 @@ namespace TechnicalTest.Core.Interfaces
     public interface IShapeFactory
     {
         Shape? CalculateCoordinates(ShapeEnum shapeEnum, Grid grid, GridValue gridValue);
+
         GridValue? CalculateGridValue(ShapeEnum shapeEnum, Grid grid, Shape shape);
     }
 }
