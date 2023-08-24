511eab65a5a86d5776421697c5ca52e0884121a4
Adam Ball <adam.ball@n-able.com>
2023-08-24 11:05:36 +0100

    updated ShapeFactory.cs to return shape from service and return grid value

--- TechnicalTest.Core/Factories/ShapeFactory.cs
+++ TechnicalTest.Core/Factories/ShapeFactory.cs
@@ -17,8 +17,8 @@ namespace TechnicalTest.Core.Factories
             switch (shapeEnum)
             {
                 case ShapeEnum.Triangle:
-                    // TODO: Return shape returned from service.
-	                return new Shape();
+                    //Returns shape returned from service.
+                    return _shapeService.ProcessTriangle(grid, gridValue);
                 default:
                     return null;
             }
@@ -29,14 +29,22 @@ namespace TechnicalTest.Core.Factories
             switch (shapeEnum)
             {
                 case ShapeEnum.Triangle:
+
                     if (shape.Coordinates.Count != 3)
                         return null;
 
-                    // TODO: Return grid value returned from service.
-                    return new GridValue(0, 0);
+                    List<Coordinate> coordinates = shape.Coordinates;
+
+                    //Returns grid value returned from service.
+                    return _shapeService.ProcessGridValueFromTriangularShape(grid, new Triangle(coordinates[1], coordinates[2], coordinates[3]));
                 default:
                     return null;
             }
+        }
+
+        Shape? IShapeFactory.CalculateCoordinates(Grid grid, GridValue gridValue)
+        {
+            throw new NotImplementedException();
         }
     }
 }
