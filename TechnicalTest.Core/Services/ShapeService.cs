616b91a9299233dcf710ba8832ff86e08b8e0937
Adam Ball <adam.ball@n-able.com>
2023-08-24 11:07:11 +0100

    updated ShapeService.cs to calcuate the results of the coordinates

--- TechnicalTest.Core/Services/ShapeService.cs
+++ TechnicalTest.Core/Services/ShapeService.cs
@@ -1,25 +1,69 @@
-﻿using TechnicalTest.Core.Interfaces;
+﻿using System;
+using TechnicalTest.Core.Interfaces;
 using TechnicalTest.Core.Models;
 
 namespace TechnicalTest.Core.Services
 {
     public class ShapeService : IShapeService
+
     {
+
+
         public Shape ProcessTriangle(Grid grid, GridValue gridValue)
         {
-            // TODO: Calculate the coordinates.
+            //Calculate the coordinates.
+
+            Coordinate topLeftVortex;
+            Coordinate outerVertex;
+            Coordinate bottomVertex;
+
+
+            //calculate left triangle
+            if (gridValue.Column % 2 != 0)
+            {
+                topLeftVortex = new Coordinate(gridValue.Column / 2 * grid.Size, (gridValue.GetNumericRow() - 1) * grid.Size);
+                outerVertex = new Coordinate(gridValue.Column / 2 * grid.Size, gridValue.GetNumericRow() * grid.Size);
+                bottomVertex = new Coordinate((gridValue.Column / 2 + 1) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+            } else //calculate right triangle
+            {
+                topLeftVortex = new((gridValue.Column / 2 - 1) * grid.Size, (gridValue.GetNumericRow() - 1) * grid.Size);
+                outerVertex = new(gridValue.Column / 2 * grid.Size, (gridValue.GetNumericRow() - 1) * grid.Size);
+                bottomVertex = new(gridValue.Column / 2 * grid.Size, gridValue.GetNumericRow() * grid.Size);
+            }
+
             return new Shape(new List<Coordinate>
             {
-                new(0, 0),
-                new(0, 0),
-                new(0, 0)
+                topLeftVortex,
+                outerVertex,
+                bottomVertex,
             });
         }
 
         public GridValue ProcessGridValueFromTriangularShape(Grid grid, Triangle triangle)
         {
-            // TODO: Calculate the grid value.
-            return new GridValue(0, 0);
+            // Calculate the grid value.
+            Coordinate OuterVertex = triangle.OuterVertex;
+            Coordinate BottomRightVertex = triangle.BottomRightVertex;
+            // OuterVertex as the base point, compare BottomRightVertex decided whether left or right triangle
+            // TopLeftVertex also fine, TopLeft.X = Outer.X -> Left, TopLeft.Y = Outer.Y -> right
+            int row = OuterVertex.Y / grid.Size;
+            int col = OuterVertex.X / grid.Size * 2;
+
+
+            // process left triangle
+
+            if (BottomRightVertex.Y == OuterVertex.Y)
+            {
+                col++;
+            }
+            // process right triangle
+            if (BottomRightVertex.X == OuterVertex.X)
+            {
+                row++;
+            }
+            return new GridValue(row, col);
         }
     }
+
+   
 }
