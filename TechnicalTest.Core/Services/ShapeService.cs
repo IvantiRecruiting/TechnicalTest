6f91573064c37f99f9406e7a770a5373c7e2b38d
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-16 12:33:55 +0100

    Added methods to calculate left triangle

--- TechnicalTest.Core/Services/ShapeService.cs
+++ TechnicalTest.Core/Services/ShapeService.cs
@@ -5,21 +5,57 @@ namespace TechnicalTest.Core.Services
 {
     public class ShapeService : IShapeService
     {
+        
+
         public Shape ProcessTriangle(Grid grid, GridValue gridValue)
         {
             // TODO: Calculate the coordinates.
-            return new Shape(new List<Coordinate>
+
+            // TODO: determine if triangle is left or right triangle based on column
+
+            if(IsLeftTriangle(gridValue.Column))
             {
-                new(0, 0),
-                new(0, 0),
-                new(0, 0)
-            });
+                return new Triangle(CalculateLeftTopLeftVertex(grid, gridValue), CalculateLeftOuterVertex(grid, gridValue), CalculateLeftBottomRightVertex(grid, gridValue));
+            }
+
+
+
+            return null;
+
+            //return new Shape(new List<Coordinate>
+            //{
+            //    new(0, 0),
+            //    new(0, 0),
+            //    new(0, 0)
+            //});
         }
 
         public GridValue ProcessGridValueFromTriangularShape(Grid grid, Triangle triangle)
         {
             // TODO: Calculate the grid value.
             return new GridValue(0, 0);
+        }
+
+   
+        private static bool IsLeftTriangle(int column)
+        {
+            return column % 2 != 0;
+        }
+
+        
+        public Coordinate CalculateLeftTopLeftVertex(Grid grid, GridValue gridValue)
+        {
+           return new Coordinate(gridValue.Column / 2 * grid.Size, (gridValue.GetNumericRow() - 1) * grid.Size);
+        }
+
+        public Coordinate CalculateLeftOuterVertex(Grid grid, GridValue gridValue)
+        {
+            return new Coordinate(gridValue.Column / 2 * grid.Size, gridValue.GetNumericRow() * grid.Size);
+
+        }
+        public Coordinate CalculateLeftBottomRightVertex(Grid grid, GridValue gridValue)
+        {
+            return new Coordinate((gridValue.Column / 2 + 1) * grid.Size, gridValue.GetNumericRow() * grid.Size);
         }
     }
 }
