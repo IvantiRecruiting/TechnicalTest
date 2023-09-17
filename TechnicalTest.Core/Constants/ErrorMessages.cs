3f220f3c94536ad989f43e1061d7db4c461df8cf
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-17 16:45:24 +0100

    Added Error Messages Constant

---
+++ TechnicalTest.Core/Constants/ErrorMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.Core.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string TriangleImplemented = "Only a Triangle is currently implemented";
+
+        public const string NoResult = "Unable to calculate result";
+
+        public const string InvalidGridValue = "Invalid Grid Value";
+    }
+}
