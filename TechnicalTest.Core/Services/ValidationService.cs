32e3dbf30aa205cbfc2a71a9821028f9bb827c4c
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-17 16:45:10 +0100

    Added Validation interface and Service

---
+++ TechnicalTest.Core/Services/ValidationService.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+using TechnicalTest.Core.Interfaces;
+
+namespace TechnicalTest.Core.Services
+{
+    public class ValidationService : IValidationService
+    {
+
+        public bool ValidateShapeTypeTriangle(ShapeEnum shapeType)
+        {
+            return shapeType != ShapeEnum.Triangle;
+        }
+
+        public bool ValidateGridValue(string gridValue)
+        {
+            return ValidateGridValueRowIsLetter(gridValue) && ValidateGridValueRowRange(gridValue) && ValidateGridColumnsIsNumber(gridValue) && ValidateGridColumnNumberRange(gridValue); 
+        }
+
+        private static bool ValidateGridValueRowIsLetter(string gridValue)
+        {
+            return char.IsLetter(gridValue, 0);
+        }
+
+        private static bool ValidateGridValueRowRange(string gridValue)
+        {
+            Regex r = new("[A-F]");
+
+            if (!r.IsMatch(gridValue)) {
+
+                return false;
+            }
+
+            return true;
+           
+        }
+
+        private static bool ValidateGridColumnsIsNumber(string gridValue)
+        {
+            var columns = gridValue.Remove(0,1);
+
+            foreach (var column in columns)
+            {
+                if(!char.IsDigit(column))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool ValidateGridColumnNumberRange(string gridValue)
+        {
+            var columns = int.Parse(gridValue.Remove(0,1));
+
+            if (!Enumerable.Range(1, 12).Contains(columns))
+            {
+                return false;
+
+            }
+
+            return true;
+        }
+    }
+}
