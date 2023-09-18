ccbc381a87b727c94a53609890ece676d3f97108
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-18 23:32:35 +0100

    Added CoordinateDTO grid value validators

---
+++ TechnicalTest.Core/Validation/GridValueColumnRange.cs
@@ -0,0 +1,20 @@
+﻿using TechnicalTest.Core.Constants;
+using TechnicalTest.Core.DTOs;
+using TechnicalTest.Core.Interfaces;
+using TechnicalTest.Core.Models;
+
+namespace TechnicalTest.Core.Validation
+{
+    public class GridValueColumnRange : ICoordinateValidator
+    {
+        public void Validate(CalculateCoordinatesDTO calculateCoordinates)
+        {
+            var columns = int.Parse(calculateCoordinates.GridValue.Remove(0, 1));
+
+            if (!Enumerable.Range(1, 12).Contains(columns))
+            {
+                throw new Exception(ErrorMessages.GridValueColumnRange);
+            }
+        }
+    }
+}
