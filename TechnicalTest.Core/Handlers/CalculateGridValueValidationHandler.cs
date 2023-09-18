299d8ecbdd1f138f435be1be00279c0517ac975c
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-18 23:33:34 +0100

    Added Validation Handlers

---
+++ TechnicalTest.Core/Handlers/CalculateGridValueValidationHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.Core.DTOs;
+using TechnicalTest.Core.Interfaces;
+using TechnicalTest.Core.Validation;
+
+namespace TechnicalTest.Core.Handlers
+{
+    public class CalculateGridValueValidationHandler : IValidationHandler
+    {
+        private CalculateGridValueDTO _dto;
+
+        public CalculateGridValueValidationHandler(CalculateGridValueDTO dto)
+        {
+            _dto = dto;
+        }
+
+
+
+        public List<IDTOValidator> commonValidators = new()
+        {
+            new ShapeType()
+        };
+
+
+        public List<IGridValueValidator> validators = new()
+        {
+            //TODO: Add a validator that checks the minimum and maximum value for the vertexs
+        };
+
+        public void Validate()
+        {
+            foreach (var validator in commonValidators)
+            {
+                validator.Validate(_dto);
+            }
+
+            foreach (var validator in validators)
+            {
+                validator.Validate(_dto);
+            }
+
+        }
+    }
+}
---
+++ TechnicalTest.Core/Handlers/CoordinatesValidationHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.Core.DTOs;
+using TechnicalTest.Core.Interfaces;
+using TechnicalTest.Core.Validation;
+
+namespace TechnicalTest.Core.Handlers
+{
+    public class CoordinatesValidationHandler : IValidationHandler
+    {
+        private CalculateCoordinatesDTO _dto;
+        public CoordinatesValidationHandler(CalculateCoordinatesDTO dto)
+        {
+            _dto = dto;
+        }
+
+
+        public List<IDTOValidator> commonValidators = new()
+        {
+            new ShapeType()
+        };
+
+        public List<ICoordinateValidator> validators = new()
+        {
+            new GridValueRowIsLetter(),
+            new GridValueRowRange(),
+            new GridValueColumnIsNumber(),
+            new GridValueColumnRange(),
+        };
+
+        public void Validate()
+        {
+            foreach (var validator in commonValidators)
+            {
+                validator.Validate(_dto);
+            }
+
+            foreach (var validator in validators)
+            {
+                validator.Validate(_dto);
+            }
+        }
+    }
+}
