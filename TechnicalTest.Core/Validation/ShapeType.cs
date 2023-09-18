32ff65f8940cd124abf37963bfb4b8ce404c3713
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-18 23:31:39 +0100

    Added ShapeType Validator

---
+++ TechnicalTest.Core/Validation/ShapeType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.Core.Constants;
+using TechnicalTest.Core.DTOs;
+using TechnicalTest.Core.Interfaces;
+using TechnicalTest.Core.Models;
+
+namespace TechnicalTest.Core.Validation
+{
+    public class ShapeType : IDTOValidator
+    {
+        public void Validate(IDTO dto)
+        {
+            switch ((ShapeEnum)dto.ShapeType)
+            {
+                case ShapeEnum.Triangle:
+                    break;
+                case ShapeEnum.None:
+                default:
+                    throw new Exception(ErrorMessages.TriangleImplemented);
+            }
+        }
+    }
+}
