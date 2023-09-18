28067b52c7d02eae1a78f917a4452e27c8bd240d
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-18 23:35:10 +0100

    Added ValidationHandlerFactory

---
+++ TechnicalTest.Core/Factories/ValidationHandlerFactory.cs
@@ -0,0 +1,24 @@
+﻿using TechnicalTest.Core.DTOs;
+using TechnicalTest.Core.Handlers;
+using TechnicalTest.Core.Interfaces;
+
+namespace TechnicalTest.Core.Factories
+{
+    public class ValidationHandlerFactory : IValidationHandlerFactory
+    {
+        public IValidationHandler GetHandler(IDTO dto)
+        {
+            if (dto is CalculateCoordinatesDTO)
+            {
+                return new CoordinatesValidationHandler((CalculateCoordinatesDTO)dto);
+            }
+
+            if (dto is CalculateGridValueDTO)
+            {
+                return new CalculateGridValueValidationHandler((CalculateGridValueDTO)dto);
+            }
+
+            throw new Exception("Handler Not Found");
+        }
+    }
+}
