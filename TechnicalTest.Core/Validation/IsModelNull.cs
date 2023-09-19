92a45e6a5dabed887338b838ee3dbaf55991af4c
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-19 12:47:40 +0100

    Added IsModelNull validator

---
+++ TechnicalTest.Core/Validation/IsModelNull.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.Core.Constants;
+using TechnicalTest.Core.Interfaces;
+
+namespace TechnicalTest.Core.Validation
+{
+    public class IsModelNull : IModelValidator
+    {
+        public void Validate(IModel model)
+        {
+            if (model == null)
+            {
+                throw new Exception(ErrorMessages.NoResult);
+            }
+        }
+    }
+}
