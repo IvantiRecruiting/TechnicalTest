4c2cda2667a37811b2b6e51c161b0491e159929e
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-19 12:03:36 +0100

    Added updated error to handler

--- TechnicalTest.Core/Handlers/CalculateGridValueValidationHandler.cs
+++ TechnicalTest.Core/Handlers/CalculateGridValueValidationHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TechnicalTest.Core.DTOs;
+﻿using TechnicalTest.Core.DTOs;
 using TechnicalTest.Core.Interfaces;
 using TechnicalTest.Core.Validation;
 
@@ -28,7 +23,7 @@ namespace TechnicalTest.Core.Handlers
 
         public List<IGridValueValidator> validators = new()
         {
-            new VertexRange()
+            new VertexRangeWhenGridSize10()
         };
 
         public void Validate()
